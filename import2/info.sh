905165ae01cd7ffdfe4ee68936d07af5f4096026
OMGboom7 <novaboom1029@gmail.com>
2022-08-06 23:22:25 +0800

    test

--- import2/info.sh
+++ import2/info.sh
@@ -1,21 +1,23 @@
 #取所需要的内网IP
-Int_ip=`ifconfig|awk '/inet addr/ {gsub(/:/," ");print $3}'|egrep "^192.168.18"|head -1`
-#取除该内网ip以及127.0.0.1之外的所有ip
-Out_ip_list=`ifconfig|awk '/inet addr/ {gsub(/:/," ");print $3}'|grep -v $Int_ip|grep -v 127.0.0.1`
+server_ip=$( ifconfig -a | grep inet | grep -v inet6 | grep -v 127.0.0.1 | awk '{print $2}')
+echo server_ip
 #取内存总值，单位为G，四舍五入
-Memory=`free -m|awk '/Mem/ {printf ("%.f\n",$2/1024)}'`
-#取内存fr
-ee值，从系统角度看，取的是第一行的free
-Memory_free=`free -m|awk '/Mem/ {printf "%.f\n",$4/1024}'`
+memory=$(free -m|awk '/Mem/ {printf ("%.f\n",$2/1024)}')
+echo memory
+#内存使用率
+memory_usage=
 #取CPU核数
-Cpu_num=`grep processor /proc/cpuinfo|wc -l`
+# shellcheck disable=SC2126
+Cpu_num=$(grep processor /proc/cpuinfo|wc -l)
+#CPU使用率
+cpu_usage=$(top -n 1 | grep Cpu |awk '{print $2}'| awk '{print int($0)}')
 #取服务器运行时间
-Uptime=`uptime|awk '{print $3}'`
-#取最近15分的负载
-Load=`uptime|awk '{print $12}'`
+Uptime=$(uptime|awk '{print $3$4}')
+#取负载
+Load=$(uptime|awk '{print $11$12$13}')
 #取磁盘大于80%的磁盘目录
-Disk=`df -h|awk '{a=+$(NF-1);if(a>=80)print $NF}'`
+Disk=$(df -h|awk '{a=+$(NF-1);if(a>=80)print $NF}')
 #swap分区总值，单位为G，四舍五入
-Swap=`free -m|awk '/Swap/ {printf ("%.f\n",$2/1024)}'`
+Swap=$(free -m|awk '/Swap/ {printf ("%.f\n",$2/1024)}')
 #swap分区
-Swap_free = `free -m|awk '/Swap/ {printf "%.f\n",$4/1024}'`
+Swap_free = $(free -m|awk '/Swap/ {printf "%.f\n",$4/1024}')
